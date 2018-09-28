d5bbde5970cc5a105f4fca038fc925bad292d207
Fabio <fabio.casati@unitn.it>
2018-09-28 11:43:29 +0200

    first api

--- api.js
+++ api.js
@@ -2,12 +2,16 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 
+
+
+var courses_offered = ['HCI','sweng']
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 
 app.get('/courses', (req, res) => {
    res.json(
-	{ projects: ['HCI','sweng'] }
+	{ courses_list: courses_offered }
 )
 })
 
